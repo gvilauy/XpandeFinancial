c1cddfa46c377b31f5e0305e56d9b4d98f36d4cf
gabriel <gabrielvila13@gmail.com>
2019-03-01 08:40:48 -0300

    OK
    Signed-off-by: gabriel <gabrielvila13@gmail.com>

--- src/org/xpande/financial/model/ValidatorFinancial.java
+++ src/org/xpande/financial/model/ValidatorFinancial.java
@@ -88,6 +88,7 @@ public class ValidatorFinancial implements ModelValidator {
                     return message;
                 }
 
+                /*
                 // Para comprobantes de compra, valido que no este asociado a una orden de pago.
                 message = this.validateInvoiceOrdenPago(model);
                 if (message != null){
@@ -100,6 +101,7 @@ public class ValidatorFinancial implements ModelValidator {
                 if (message != null){
                     return message;
                 }
+                */
             }
 
         }
